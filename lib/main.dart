923c8f401b1d242668d204fc1f4495f19561b2de
Felipe <felipedelucanogueira@gmail.com>
2021-05-24 03:30:55 -0300

    Formulario com validaçao finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final _formKey = GlobalKey<FormState>();
+  String name = '';
+  String lastName = '';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints(
+              maxHeight: 500, maxWidth: 450, minWidth: 300, minHeight: 300),
+          padding: EdgeInsets.only(left: 20, right: 20),
+          width: MediaQuery.of(context).size.width * 0.7,
+          height: MediaQuery.of(context).size.height * 0.5,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
           ),
-        ) ,
-      )
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Nome'),
+                      onChanged: (text) {
+                        name = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Esse campo é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: TextFormField(
+                      decoration: InputDecoration(hintText: "Sobrenome"),
+                      onChanged: (text) {
+                        lastName = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Esse campo é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          final isValid = _formKey.currentState!.validate();
+
+                          if (isValid) {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    content:
+                                        Text("Olá ${name + ' ' + lastName}"),
+                                  );
+                                });
+                          }
+                        },
+                        child: Text('Validar')),
+                  )
+                ],
+              )),
+        ),
+      ),
     );
   }
 }
