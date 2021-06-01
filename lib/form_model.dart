af1066cde846c413af1f2098681133de618c0eb4
Felipe <felipedelucanogueira@gmail.com>
2021-06-01 16:11:55 -0300

    Padrão Adapter Implementado

--- lib/form_model.dart
+++ lib/form_model.dart
@@ -1,28 +1,28 @@
 import 'package:form_adaptativo/internal_storage.dart';
+import 'package:form_adaptativo/secure_storage_adapter.dart';
+import 'package:form_adaptativo/shared_preferences_adapter.dart';
 
 class FormModel{
   String? name;
   String? surname;
 
-
-
   // set setName(String valor){
   //   this.name = valor;
   //
   // }
+  final InternalStorageAdapter internalStorage;
 
-  final InternalStorage internalStorage = InternalStorage();
+  FormModel({InternalStorageAdapter? internalStorageAdapter}) : internalStorage = internalStorageAdapter ?? SharedPreferencesAdapter();
 
   void saveUser(){
     if (name == null) return;
     if (surname == null) return;
-   //internalStorage.saveUser(name!, surname!);
-   internalStorage.secureUser(name!, surname!);
 
+    internalStorage.saveUser(name!, surname!);
   }
 
   Future<String> getFullName(){
-  //  return internalStorage.getUserFullName();
-    return internalStorage.getSecureFullName();
+    return internalStorage.getFullName();
+
   }
 }
--- lib/internal_storage.dart
+++ lib/internal_storage.dart
@@ -1,45 +1,4 @@
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-
-class InternalStorage{
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  final FlutterSecureStorage secureStorage = FlutterSecureStorage();
-
-  void saveUser(String name, String surname) async {
-    final internalPref = await _prefs;
-    internalPref.setString('name', name);
-    internalPref.setString('surname', surname);
-    print('Dados Salvos');
-
-  }
-
-  Future <String> getUserFullName() async {
-    final internalPref = await _prefs;
-    String? name = internalPref.getString('name');
-    String? surname = internalPref.getString('surname');
-
-    if((name != null) && (surname != null)){
-    return name + ' ' + surname;
-    }else{
-      return 'Usuário nao Salvo';
-    }
-  }
-  void secureUser(String name , String surname) async{
-    await secureStorage.write(key: 'name', value: name);
-    await secureStorage.write(key: 'surname', value: surname);
-    print('Dados Seguros');
-
-
-    }
-  Future <String> getSecureFullName() async{
-    String? name = await secureStorage.read(key: 'name');
-    String? surname = await secureStorage.read(key: 'surname');
-
-    if((name != null) && (surname != null)){
-      return name + ' ' + surname;
-    }else{
-      return 'Dados não Seguros';
-    }
-  }
+abstract class InternalStorageAdapter{
+  void saveUser(String name , String surname);
+  Future<String> getFullName();
 }
